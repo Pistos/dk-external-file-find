a8ec94855401147cdb5e9c39214809012e4b8d8a
Pistos <git dawt pistos aet purepistos dawt net>
2023-03-07 18:17:13 -0500

    Update extension info

--- external-file-find.rb
+++ external-file-find.rb
@@ -1,21 +1,77 @@
-module Diakonos
-  module Functions
+require "shellwords"
 
-    def open_with_file_finder(finder_executable)
-      Curses::close_screen
+# Replace Diakonos' built-in class
+class FuzzyFileFinder
+  LIST_FILE = File.join(
+    $diakonos.diakonos_home,
+    "external-file-find--files.txt"
+  )
+  TEMP_FILE = LIST_FILE + ".tmp"
 
-      file = `find #{@session.dir} -type f | #{finder_executable}`
-      file&.strip!
+  def initialize(
+    ceiling:,  # ignored
+    directories:,
+    ignores:,  # TODO
+    recursive:  # ignored
+  )
+    @ignores = Array(ignores)
 
-      Curses::init_screen
-      refresh_all
+    dirs = Array(directories)
 
-      if file && ! file.empty?
-        open_file file
-        update_status_line
-        update_context_line
-      end
+    if dirs.empty?
+      dirs << "."
     end
 
+    # @files = []
+    # @directories = {}  # To detect link cycles
+    # @dirs_with_many = []
+
+    write_file_list(dirs: dirs)
+  end
+
+  def find(query, max = nil)
+    finder = (
+      $diakonos
+      .settings["extension.external_file_finder.command"]
+      .gsub(
+        /\$s/,
+        Shellwords.escape(query)
+      )
+    )
+
+    `#{finder} < #{LIST_FILE}`
+    .split("\n")
+    .map { |path|
+      {path: path}
+    }
+  end
+
+  private def write_file_list(dirs:)
+    File.open(LIST_FILE, "w") do |f|
+      dir_list = dirs.join(" ")
+      `find #{dir_list} -type f > #{TEMP_FILE}`
+
+      File.open(TEMP_FILE, "r").each_line do |path|
+        path_ = path.strip
+
+        dirs.each do |dir|
+          if dir.end_with?("/")
+            dir_ = dir
+          else
+            dir_ = dir + "/"
+          end
+
+          if path_.start_with?(dir_)
+            path_ = path_[dir_.length..-1]
+          end
+        end
+
+        if @ignores.none? { |pattern|
+          File.fnmatch(pattern, path_)
+        }
+          f.puts path_
+        end
+      end
+    end
   end
 end
